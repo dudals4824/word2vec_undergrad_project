71ceb2198d6dce16e7cec959f94cb68caf7d9a0d
dudals4824 <dudals4824@gmail.com>
2018-09-10 15:02:29 +0900

    수정
    수정

--- youngmin_source/cnnUtils.py
+++ youngmin_source/cnnUtils.py
@@ -1,53 +1,27 @@
 import numpy as np
 import pandas as pd
-import re, sys
-from gensim.models import Word2Vec
 
-def clean_str(string):
-    """
-    Tokenization/string cleaning for all datasets except for SST.
-    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
-    """
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-    string = re.sub(r"\'s", " \'s", string)
-    string = re.sub(r"\'ve", " \'ve", string)
-    string = re.sub(r"n\'t", " n\'t", string)
-    string = re.sub(r"\'re", " \'re", string)
-    string = re.sub(r"\'d", " \'d", string)
-    string = re.sub(r"\'ll", " \'ll", string)
-    string = re.sub(r",", " , ", string)
-    string = re.sub(r"!", " ! ", string)
-    string = re.sub(r"\(", " \( ", string)
-    string = re.sub(r"\)", " \) ", string)
-    string = re.sub(r"\?", " \? ", string)
-    string = re.sub(r"\s{2,}", " ", string)
-    return string.strip().lower()
-
-def next_batch(start, batch_size):
+def next_batch(start, batch_size, traindf, wv):
     # start : epoch에서의 index를 뜻함.
     batch_xs = list()
     batch_ys = list()
-    traindf = pd.read_csv("./data/rt-polaritydata/tt.csv")
-    model = Word2Vec.load("./data/rt-polaritydata/wvModel")
-    wv = model.wv
-    for row in traindf.itertuples(index=True):
 
-        if start <= int(row[0]) <= start + batch_size - 1:
-            # 받은 row들을 1 X 300 데이터로 바꿔줘야한다.
-            x = []
-            seq = getattr(row, "sequence")
-            seq = clean_str(seq)
-            seq = seq.split(" , ")
+    for i in range(start, start+batch_size):
+        arr = traindf.loc[i].values
+        arr = arr[0].split(" ")
+        x = []
 
-            for elem in seq:
-                x.append(wv.vectors[int(elem)])
-            batch_xs.append(x)
+        y = np.zeros(50095, dtype="int32")
+        y[int(arr[11])] = 1
+        batch_ys.append(y)
 
-            y = np.zeros(50095, dtype="int32")
-            y[int(row[2])] = 1
-            batch_ys.append(y)
+        for idx in range(0, 11):
+            if idx == 5:
+                continue
+            x.append(wv.vectors[int(arr[idx])])
+        batch_xs.append(x)
 
-        if int(row[0]) >= start + batch_size - 1:
 
-            start = start + batch_size
-            return start, batch_xs, batch_ys
+    start = start + batch_size
+
+    return start, batch_xs, batch_ys
--- youngmin_source/newcassifier.py
+++ youngmin_source/newcassifier.py
@@ -1,20 +1,24 @@
 import tensorflow as tf
+import pandas as pd
+import time
+from gensim.models import Word2Vec
 from cnnClassifier import cnnUtils
 
 tf.set_random_seed(777)
 
 learning_rate = 0.001
-training_epochs = 20
-batch_size = 16
+training_epochs = 1
+batch_size = 64
+train_len = pd.read_csv("./data/rt-polaritydata/data.csv").size / 2
 
 keep_prob = tf.placeholder(tf.float32)
 
 # 이전에 데이터 들어가는 부분이 있어야함.
 # 여기서 None은 몇 라인이 들어갈지 정해질거임
-X = tf.placeholder(tf.float32, [batch_size, 11, 300], name="x_input")
+X = tf.placeholder(tf.float32, [batch_size, 10, 300], name="x_input")
 # X를 받는대로 reshape를 하면 단어 11개에 대해서 300차원으로 표현되고
 # -1은 개수로 들어갈거임 None이랑 비슷한 의미
-X_reshape = tf.reshape(X, [-1, 11, 300, 1])
+X_reshape = tf.reshape(X, [-1, 10, 300, 1])
 
 # 우리가 softmax하고 싶은 종류는 총 50095개
 # 답이 50095개니까 50095개로
@@ -29,9 +33,6 @@ with tf.name_scope("layer1") as scope:
     L1 = tf.nn.max_pool(L1, ksize=[1, 3, 300, 1], strides=[1, 2, 2, 1], padding='SAME')
     L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
-    W1_hist = tf.summary.histogram("Weight1", W1)
-    L1_hist = tf.summary.histogram("Layer1", L1)
-
 with tf.name_scope("layer2") as scope:
     W2 = tf.Variable(tf.random_normal([4, 300, 32, 64], stddev=0.01), name="Weight2")
     L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
@@ -39,18 +40,12 @@ with tf.name_scope("layer2") as scope:
     L2 = tf.nn.max_pool(L2, ksize=[1, 4, 300, 1], strides=[1, 2, 2, 1], padding='SAME')
     L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-    W2_hist = tf.summary.histogram("Weight2", W2)
-    L2_hist = tf.summary.histogram("Layer2", L2)
-
 with tf.name_scope("layer3") as scope:
     W3 = tf.Variable(tf.random_normal([5, 300, 64, 128], stddev=0.01), name="Weight3")
     L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
     L3 = tf.nn.relu(L3)
     L3 = tf.nn.max_pool(L3, ksize=[1, 4, 300, 1], strides=[1, 2, 2, 1], padding='SAME')
     L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
-
-    W3_hist = tf.summary.histogram("Weight3", W3)
-    L3_hist = tf.summary.histogram("Layer3", L3)
 
     L3_flat = tf.reshape(L3, [-1, 2*38*128])
 
@@ -61,9 +56,6 @@ with tf.name_scope("layer4") as scope:
     L4 = tf.nn.relu(tf.matmul(L3_flat, W4) + b4)
     L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
-    b4_hist = tf.summary.histogram("Bias4", b4)
-    L4_hist = tf.summary.histogram("Layer4", L4)
-
 with tf.name_scope("layer5") as scope:
     W5 = tf.get_variable("W5", shape=[625, 50095],
                      initializer=tf.contrib.layers.xavier_initializer())
@@ -73,12 +65,19 @@ with tf.name_scope("layer5") as scope:
     b5_hist = tf.summary.histogram("Bias5", b5)
     logits_hist = tf.summary.histogram("Logits", logits)
 
-
 with tf.name_scope("cost/loss") as scope:
+    # W5 :: [625,50095]
+    # b5 :: [50095]
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+    # cost = tf.reduce_mean(tf.nn.nce_loss(weights=tf.transpose(W5), biases=b5, inputs=L4, num_sampled=32, num_classes=50095, labels=Y))
 
 with tf.name_scope("train") as scope:
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+with tf.name_scope("accuracy") as scope:
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
 
 sess = tf.Session()
 # tensorboard --logdir=./logs/xor_logs
@@ -88,27 +87,34 @@ writer.add_graph(sess.graph)
 saver = tf.train.Saver()
 sess.run(tf.global_variables_initializer())
 
+traindf = pd.read_csv("./data/rt-polaritydata/newdata.csv")
+model = Word2Vec.load("./data/rt-polaritydata/wvModel")
+wv = model.wv
+
 print("Learning started, It takes long.")
 for epoch in range(training_epochs):
     avg_cost = 0
     start = 0
     # 임의로 초기화
-    train_len = 32
     total_batch = int(train_len / batch_size)
     for i in range(0, total_batch):
-        start, batch_xs, batch_ys = cnnUtils.next_batch(start, batch_size)
+        print(str(i+1)+"/"+str(total_batch))
+        stime = time.time()
+        start, batch_xs, batch_ys = cnnUtils.next_batch(start, batch_size, traindf, wv)
+        print(str(time.time() - stime))
         feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.5}
-        summary, c, _ = sess.run([merged_summary, cost, optimizer], feed_dict=feed_dict)
+        summary, c, acc, _ = sess.run([merged_summary, cost, accuracy, optimizer], feed_dict=feed_dict)
         writer.add_summary(summary, global_step=i)
         avg_cost += c / total_batch
-    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+        print(str(time.time() - stime))
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost), 'accuarcy= ', '{:.9f}'.format(accuracy))
 
 saver.save(sess, './runs')
 print("Learning Finished!")
 
 
-start = 0
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-start, batch_xs, batch_ys = cnnUtils.next_batch(start, batch_size)
-print('Accuracy:', sess.run(accuracy, feed_dict={X: batch_xs, Y: batch_ys, keep_prob: 1}))
+# start = 0
+# correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# start, batch_xs, batch_ys = cnnUtils.next_batch(start, batch_size)
+# print('Accuracy:', sess.run(accuracy, feed_dict={X: batch_xs, Y: batch_ys, keep_prob: 1}))
